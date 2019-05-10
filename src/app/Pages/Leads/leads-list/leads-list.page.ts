b37004af2229d2a927838a42697725fb605492e8
hyd3r <hyd3r.exe@gmail.com>
2019-05-10 10:57:12 +0800

    Changed searchbar and modified cliend model

--- src/app/Pages/Leads/leads-list/leads-list.page.ts
+++ src/app/Pages/Leads/leads-list/leads-list.page.ts
@@ -8,29 +8,35 @@ import { LeadsClientModel } from '../leads/leads-client.model';
 })
 export class LeadsListPage implements OnInit {
   @Input() clients: LeadsClientModel[] = [{
+    id:1,
     company: 'SDT - Core',
     client: 'Jigo',
     position: 'OJT'
   },
 {
+  id:2,
   company: 'University of Mindanao',
   client: 'Karl',
   position: 'Dad'
 },
 {
+  id:3,
   company: 'Test Company',
   client: 'Toto',
   position: 'CEO'
 },
 {
+  id:4,
   company: 'League of Buses',
   client: 'Jaiam',
   position: 'President'
 },{
+  id:5,
   company: 'Capcom',
   client: 'Cris',
   position: 'Director'
 },{
+  id:6,
   company: 'Dummy Company',
   client: 'Thes',
   position: 'Kid'
