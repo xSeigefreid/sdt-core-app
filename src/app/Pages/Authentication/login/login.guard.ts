bcff3a3414e35b7e36e774977947fa74364535b9
denver3542 <denver3542@gmail.com>
2019-05-14 13:03:38 +0800

    Added new Design and authentication security i.e auth guard

---
+++ src/app/Pages/Authentication/login/login.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { CanLoad, Route, UrlSegment, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { LoginService } from './login.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginGuard implements CanLoad {
+  
+  constructor(private loginService: LoginService, private router: Router){}
+
+  canLoad(route: Route, 
+    segments: UrlSegment[]
+    ): Observable<boolean> | Promise<boolean> | boolean {
+      if (!this.loginService.userIsAuthenticated){
+        this.router.navigateByUrl('/login');
+      }
+      return this.loginService.userIsAuthenticated;
+    }
+}
