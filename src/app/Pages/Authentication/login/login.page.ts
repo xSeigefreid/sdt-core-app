3ff2b87a2d4c6b2a4599608f5f7afa191bd950bf
denver3542 <denver3542@gmail.com>
2019-05-17 09:37:19 +0800

    Added new Design and authentication security i.e auth guard

--- src/app/Pages/Authentication/login/login.page.ts
+++ src/app/Pages/Authentication/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from './login.service';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  
-  constructor() { }
+  isLogin = true;
+  constructor(private loginService: LoginService, private router: Router) { }
 
   ngOnInit() {
   }
-
+  onLogin() {
+    this.loginService.login();
+    this.router.navigateByUrl('/leads');
+  }
+onSubmit(form: NgForm) {
+  if(!form.valid) {
+    return;
+  }
+  const email = form.value.email;
+  const pass = form.value.pass;
+  console.log(email,pass);
+  if(this.isLogin) {
+    //send request to login server
+  }else {
+    //send request to sign up serve
+  }
+}
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { LoginGuard } from './Pages/Authentication/login/login.guard';
 
 const routes: Routes = [
   {
@@ -7,18 +8,18 @@ const routes: Routes = [
     redirectTo: 'login',
     pathMatch: 'full'
   },
-  { path: 'leads', loadChildren: './Pages/Leads/leads/leads.module#LeadsPageModule' },
-  { path: 'calendar', loadChildren: './Pages/Calendar/calendar/calendar.module#CalendarPageModule' },
-  { path: 'report', loadChildren: './Pages/Reports/report/report.module#ReportPageModule' },
-  { path: 'login', loadChildren: './Pages/Authentication/login/login.module#LoginPageModule' },
-  { path: 'display', loadChildren: './Pages/Profile/display/display.module#DisplayPageModule' },
+  { path: 'leads', loadChildren: './Pages/Leads/leads/leads.module#LeadsPageModule', canLoad: [LoginGuard] },
+  { path: 'calendar', loadChildren: './Pages/Calendar/calendar/calendar.module#CalendarPageModule', canLoad: [LoginGuard] },
+  { path: 'report', loadChildren: './Pages/Reports/report/report.module#ReportPageModule', canLoad: [LoginGuard] },
+  { path: 'login', loadChildren: './Pages/Authentication/login/login.module#LoginPageModule'},
+  { path: 'display', loadChildren: './Pages/Profile/display/display.module#DisplayPageModule', canLoad: [LoginGuard] },
 
   // to be removed
-  { path: 'leads-list', loadChildren: './Pages/Leads/leads-list/leads-list.module#LeadsListPageModule' },
-  { path: 'lapsed-list', loadChildren: './Pages/Leads/lapsed-list/lapsed-list.module#LapsedListPageModule' },
-  { path: 'upcoming-list', loadChildren: './Pages/Leads/upcoming-list/upcoming-list.module#UpcomingListPageModule' },
+  { path: 'leads-list', loadChildren: './Pages/Leads/leads-list/leads-list.module#LeadsListPageModule', canLoad: [LoginGuard] },
+  { path: 'lapsed-list', loadChildren: './Pages/Leads/lapsed-list/lapsed-list.module#LapsedListPageModule', canLoad: [LoginGuard] },
+  { path: 'upcoming-list', loadChildren: './Pages/Leads/upcoming-list/upcoming-list.module#UpcomingListPageModule', canLoad: [LoginGuard] },
   // { path: 'edit-form', loadChildren: './Pages/Leads/edit-form/edit-form.module#EditFormPageModule' },
-  { path: 'edit-form', loadChildren: './Pages/Profile/edit-form/edit-form.module#EditFormPageModule' },
+  { path: 'edit-form', loadChildren: './Pages/Profile/edit-form/edit-form.module#EditFormPageModule', canLoad: [LoginGuard] },
 ];
 
 @NgModule({
